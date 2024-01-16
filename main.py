648ccaf56acbc5af8ceb9a85e6f893cd02d94df6
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2024-01-16 17:15:52 +0100

    Fixes

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 #Code by Sergio 1260
 
-"""
-
-BASIC FTP via WEB with a basic interface
-Allows you to share a folder across the LAN
-(READ-ONLY mode)
-
-CAPABILITIES:
-
- - Can play videos
- - Can play music
- - Can read pdf
- - Can read plain text
- - If nothing of above it downloads it
-
-"""
+# BASIC FTP via WEB with a basic interface
+# Allows you to share a folder across the LAN
+# (READ-ONLY mode)
 
 from functions import *
+from actions import *
+from flask import Flask, render_template, request, send_from_directory
 
 if __name__=="__main__":
-    port, listen, root, debug = init()
+    port, listen, root, debug, folder_size = init()
     app = Flask(__name__)
 
 # Default file page
@@ -27,7 +17,7 @@ if __name__=="__main__":
 def file_page():
     try:
         path=request.args['path']
-        directory, file=fix_Addr(path)
+        directory, file=fix_Addr(path,root)
         if directory==None: return render_template('403.html'), 403
         elif not exists(directory+sep+file):
             return render_template('404.html'), 404
@@ -50,27 +40,7 @@ def video_page():
 def audio_page():
     try:
         path=request.args['path']
-        print(path)
-        folder=sep.join(path.split(sep)[:-1])
-        name=path.split(sep)[-1]; lst=[]
-        out=get_folder_content(root+sep+folder)
-        for x in out:
-            if x["description"]=="Audio": lst.append(x["path"])
-
-        # Get previous song
-        try: nxt=lst[lst.index(path)+1]
-        except: nxt=lst[0]
-
-        # Get next song
-        if lst.index(path)==0: prev=lst[-1]
-        else: prev=lst[lst.index(path)-1]
-        
-        # The {{ url_for('audio_page', path=nxt} inside the html does
-        # a weird thing with the ' char, fixed with this code
-        filepg="/audio/?path="
-        nxt=filepg+nxt.replace("'","%27").replace("&","%26")
-        prev=filepg+prev.replace("'","%27").replace("&","%26")
-        
+        prev, nxt, name, path = audio(path,root)
         if not exists(root+sep+path): return render_template('404.html'), 404
         if not access(root+sep+path, R_OK): return render_template('403.html'), 403
         return render_template('audio.html', path=path, name=name,prev=prev, nxt=nxt )
@@ -81,7 +51,7 @@ def audio_page():
 def text_page():
     try:
         path=request.args['path']
-        directory, file=fix_Addr(path)
+        directory, file=fix_Addr(path,root)
         if not exists(root+sep+path): return render_template('404.html'), 404
         if not access(root+sep+path, R_OK): return render_template('403.html'), 403
         return send_from_directory(directory,file,mimetype='text')
@@ -90,26 +60,11 @@ def text_page():
 @app.route('/')
 def index():
     try:
-        is_root=False
-        if 'path' not in request.args:
-            folder_path=root; is_root=True
-        else:
-            folder_path=request.args['path']
-            if folder_path=="." or folder_path=="": is_root=True
-            folder_path=root+sep+folder_path
-        if not exists(folder_path): return render_template('404.html'), 404
-        if not access(folder_path, R_OK): return render_template('403.html'), 403
-        # Deny access if not inside root
-        if not is_subdirectory(root, abspath(folder_path)): raise PermissionError
-        folder_content = get_folder_content(folder_path)
-        parent_directory = abspath(join(folder_path, pardir))
-        if parent_directory==root: parent_directory=""
-        else: parent_directory= relpath(parent_directory, start=root)
-        folder_path = relpath(folder_path, start=root)
-        if folder_path==".": folder_path=""
-        folder_path="/"+folder_path.replace(sep,"/")
-        return render_template('index.html', folder_content=folder_content,folder_path=folder_path,parent_directory=parent_directory,is_root=is_root)
+        folder_content,folder_path,parent_directory,is_root=index_func(request.args,root,folder_size)
+        return render_template('index.html', folder_content=folder_content,folder_path=folder_path,
+                               parent_directory=parent_directory,is_root=is_root)
     except PermissionError: return render_template('403.html'), 403
+    except FileNotFoundError: return render_template('404.html'), 404
     except: return render_template('500.html'), 500
 
 if __name__=="__main__": app.run(host=listen, port=int(port), debug=debug)
