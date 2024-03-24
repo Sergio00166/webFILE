1df1cb0b640933f03fa3a0dc6bbfb06ea093996a
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2024-03-24 14:00:29 +0100

    More functionality

--- start.py
+++ start.py
@@ -4,9 +4,20 @@ from subprocess import Popen
 from sys import path, argv
 from os.path import exists, isdir
 from os import sep
+from threading import Thread
+from time import sleep as delay
+
+def worker(ip,port,folder_size,root):
+    PyExec=path[0]+sep+"bin"+sep+"main.py"
+    if sep==chr(92): python="python"
+    else: python="python3"
+    args=[python,PyExec,"-b",ip,"-p",port,"-d",root]
+    if folder_size: args.append("--dirsize")
+    process = Popen(args)
 
 def init():
-    if len(argv)==1: file=path[0]+sep+"bin"+sep+"config.cfg"
+    if len(argv)==1:
+        file=path[0]+sep+"bin"+sep+"config.cfg"
     else: file=argv[1]
     try: file = open(file,"r")
     except: print("ERROR: config file not valid or not exists"); exit()
@@ -14,11 +25,8 @@ def init():
     for x in file:
         x=x.rstrip().lstrip()
         if not len(x)==0 and not x.startswith("#"):
-            key=x[:x.find(":")]
-            value=x[x.find(":")+1:]
-            value=value.rstrip().lstrip()
-            key=key.rstrip().lstrip()
-            dic[key]=value
+            key=x[:x.find(":")]; value=x[x.find(":")+1:]
+            value=value.strip(); dic[key.strip()]=value
     if not "port" in dic: dic["port"]="80"
     if not "listen" in dic: dic["listen"]="172.0.0.1"
     if not "show.folder.size" in dic: folder_size="false"
@@ -28,18 +36,26 @@ def init():
     root=dic["folder"]
     if not (exists(root) and isdir(root)):
         print("[CFG_FILE]: THE SPECIFIED FOLDER PATH IS NOT VALID"); exit()
-        
-    return dic["port"], dic["listen"], root, folder_size
+    port=dic["port"]; listen=dic["listen"]
+    if "-" in port:
+        st,end = port.split("-")
+        st=int(st); end=int(end)
+        ports=[str(x) for x in range(st,end+1)]
+    else: ports=[x.strip() for x in port.split(",")]
+    listen=[x.strip() for x in listen.split(",")]
+    return ports, listen, root, folder_size
 
 def main():
-    PyExec=path[0]+sep+"bin"+sep+"main.py"
-    port, listen, root, folder_size = init()
-    if sep==chr(92): python="python"
-    else: python="python3"
-    args=[python,PyExec,"-b",listen,"-p",port,"-d",root]
-    if folder_size: args.append("--dirsize")
-    process = Popen(args)
-    try: process.wait()
-    except: pass
+    threads=[]
+    ports, listen, root, folder_size = init()
+    for ip in listen:
+        for port in ports:
+            threads.append(Thread(target=worker,
+                args=(ip,port,folder_size,root,)))
+    for x in threads: x.start(); delay(0.1)
+    try: # wait
+        while True: delay(1)
+    except: exit()
+
 
 if __name__=="__main__": main()
