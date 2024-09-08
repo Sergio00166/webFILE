3ed188dc7280e77f413f3685df7d61d2df783ca8
Sergio Bañón <35770464+Sergio00166@users.noreply.github.com>
2024-09-08 16:49:01 +0200

    fix

--- bin/actions.py
+++ bin/actions.py
@@ -45,7 +45,7 @@ def init():
     return args.port, args.bind, args.dir, args.dirsize
 
 
-def filepage_func(path,root,filetype,random=False,nonxt=False):
+def filepage_func(path,root,filetype,random=False):
     # Get relative path from the root dir
     path=relpath(isornot(path,root), start=root)
     # Get the name of the folder
@@ -57,14 +57,14 @@ def filepage_func(path,root,filetype,random=False,nonxt=False):
     path=path.replace(sep,"/")
     # Get all folder contents that has the same filetype
     lst = [x["path"] for x in out if x["description"] == filetype]
-    # Get previous one
+    # Get next one
     try: nxt=lst[lst.index(path)+1]
     except: nxt=lst[0]
-    # Get next one
+    # Get previous one
     if lst.index(path)==0: prev=lst[-1]
     else: prev=lst[lst.index(path)-1]
     # Fix url strings
-    nxt = "" if nonxt else fix_pth_url(nxt)
+    nxt = fix_pth_url(nxt)
     prev = fix_pth_url(prev)
     # Return random flag
     if random:
