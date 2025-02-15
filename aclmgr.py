e895ee7da178cc6f8d225a2638fd3f81e6928cb5
Sergio Bañón <35770464+Sergio00166@users.noreply.github.com>
2025-02-15 16:10:41 +0100

    Update aclmgr.py

--- aclmgr.py
+++ aclmgr.py
@@ -11,9 +11,17 @@ from sys import path
 try: import readline
 except: pass
 
-users_file  = getenv('USERS_FILE',path[0]+sep+"users.json")
-acl_file    = getenv('ACL_FILE'  ,path[0]+sep+"acl.json")
+users_file  = getenv('USERS_FILE',None)
+acl_file    = getenv('ACL_FILE'  ,None)
 USERS,ACL   = {},{}
+
+if not all((users_file, acl_file)):
+    data_dir = path[0]+sep+"data"+sep
+    makedirs(data_dir, exist_ok=True)
+    
+users_file  = users_file  or (data_dir+"users.json")
+acl_file    = acl_file    or (data_dir+"acl.json")
+
 
 """ LOAD DATA FROM DISK """
 try:
