3ad8c5c9b0f95c9a588d52b23516ecc5f287c2bb
Sergio Bañón <35770464+Sergio00166@users.noreply.github.com>
2025-04-01 18:31:39 +0200

    Update actions.py

--- app/actions.py
+++ app/actions.py
@@ -1,8 +1,8 @@
 #Code by Sergio00166
 
+from video import get_subtitles,get_chapters,get_info,check_ffmpeg_installed
 from flask import render_template,stream_template,redirect,request
 from files_mgr import upfile,updir,mkdir,delfile,move,copy
-from video import get_subtitles,get_chapters,get_info
 from os.path import join, relpath,pardir,abspath
 from urllib.parse import quote as encurl
 from send_file import send_file,send_dir
