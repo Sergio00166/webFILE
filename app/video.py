e618411db9ccab911b888c2e7c95e38c07b5caf6
Sergio Bañón <35770464+Sergio00166@users.noreply.github.com>
2025-04-04 13:11:07 +0200

    Minor fix

--- app/video.py
+++ app/video.py
@@ -68,30 +68,32 @@ def ffmpeg_extract_info(file_path,sz,mt):
 
 
 @cache.cached("sz","mt")
-def ffmpeg_get_subs(file,index,codec,legacy,sz,mt):
+def ffmpeg_get_subs(file,index,legacy,sz,mt):
+    if legacy:
+        ass2vtt = get_codec(file,index)=="ass"
+        codec = "ass" if ass2vtt else "webvtt"
+    else: ass2vtt,codec = False,"ass"
+  
     out = run( [
         'ffmpeg', '-i', file, '-map',
         f'0:s:{index}', '-f', codec, '-'
     ], stdout=PIPE,stderr=PIPE )
 
     if out.returncode != 0:
-        raise NotImplementedError(
-            f"Unsupported subtitle codec"
-        )
+        raise NotImplementedError(f"Unsupported subtitle codec")
+
     out = out.stdout.decode()
-    if legacy and codec!="webvtt":
-        return convert_ssa(out)
-    return out
+    return convert_ssa(out) if ass2vtt else out
 
 
 
 # sz & mt are just to invalidate cache
 
-def extract_subtitles(index,file,codec,legacy):
+def extract_subtitles(index,file,legacy):
     sname = ".".join(file.split(".")[:-1]+["mks"])
     if exists(sname) and isfile(sname): file = sname
     sz,mt = getsize(file),getmtime(file)
-    args = (file,index,codec,legacy,sz,mt)
+    args = (file,index,legacy,sz,mt)
     return ffmpeg_get_subs(*args)
 
 def get_info(file):
@@ -105,15 +107,22 @@ def get_chapters(file):
     return ffmpeg_extract_chapters(file,sz,mt)
 
 
+def get_codec(source,index):
+    return "ass" if run([
+        'ffprobe', '-v', 'quiet',
+        '-select_streams', f's:{index}',
+        '-show_entries', 'stream=codec_name','-of',
+        'default=noprint_wrappers=1:nokey=1',source
+    ], stdout=PIPE,stderr=PIPE).stdout.decode().strip() \
+    in ["ssa","ass"] else "webvtt"
+
+
 def get_subtitles(index,file,legacy):
-    # Set codec and mimetype for the subtitles
-    if legacy: mime,codec = "text/vtt","webvtt"
-    else: mime,codec = "application/x-substation-alpha","ass"
-    # Extract from the video or exrernal container
-    out = extract_subtitles(index,file,codec,legacy)
+    out = extract_subtitles(index,file,legacy)
     # Get filename and for downloading the subtitles
     subsname = file.split(sep)[-1]+f".track{str(index)}."
-    subsname += "vtt" if codec=="webvtt" else codec
+    subsname += "vtt" if legacy else "ssa"
+    mime = "text/vtt" if legacy else "application/x-substation-alpha"
     headers = {'Content-Disposition': 'attachment;filename='+subsname}
     # Return the subtittle track with the right mime
     return Response(out, mimetype=mime, headers=headers)
