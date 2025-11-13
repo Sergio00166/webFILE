0f5d1b293964eca7109ef6d8dd003618020fb345
Sergio Bañón <35770464+Sergio00166@users.noreply.github.com>
2025-11-13 16:00:24 +0100

    Fix root handling

--- app/functions.py
+++ app/functions.py
@@ -47,7 +47,10 @@ def validate_acl(path, ACL, write=False):
     askd_perm = 2 if write else 1
     user = session.get("user", "DEFAULT")
     path = normpath(path)
-    if path == ".": path = "/"
+
+    # Always allow root
+    if path == ".": return
+
     path = path.replace(sep, "/")
     prop = False
 
